6c967b696c1aab778ce4fdbbab4f9d6b123ff2e4
KareemND <119475435+KareemND@users.noreply.github.com>
2023-06-01 14:03:19 -0400

    edited paths for images and changed the favicon

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,13 +5,13 @@ function Portfolio() {
   const projects = [
     { 
       title: "Melody Match", 
-      imageUrl: "/logo.png", 
+      imageUrl: process.env.PUBLIC_URL + "/logo.png", 
       demoUrl: "https://team-2-project.github.io/Melody-Match/", 
       codeUrl: "https://github.com/team-2-project/Melody-Match" 
     },
     { 
       title: "Vet-Set-Go", 
-      imageUrl: "/vsg-logo.png", 
+      imageUrl: process.env.PUBLIC_URL + "/vsg-logo.png", 
       demoUrl: "https://whispering-coast-03143.herokuapp.com/", 
       codeUrl: "https://github.com/Project2Group1/vet-set-go" 
     },
